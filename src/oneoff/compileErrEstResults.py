97cea596531be98f398d00c33d82a1ff00845ce7
willa099@umn.edu <willa099@umn.edu>
2021-07-06 10:25:27 -0500

    move to remote

--- src/oneoff/compileErrEstResults.py
+++ src/oneoff/compileErrEstResults.py
@@ -37,7 +37,8 @@ for k in folds_arr: #CHANGE DIS----------------
 	# ea_df = pd.read_feather("../../results/err_est_outputs_225hid_EALSTM_fold"+str(k)+".feather")
 	# ea_df = pd.read_feather("../../results/err_est_outputs_1layer256hid_2.4rmse_EALSTM_fold"+str(k)+".feather")
 	# ea_df = pd.read_feather("../../results/err_est_outputs_2layer128hid_2.4rmse_EALSTM_fold"+str(k)+".feather")
-	ea_df = pd.read_feather("../../results/err_est_outputs_070221_EALSTM_fold"+str(k)+".feather")
+	ea_df = pd.read_feather("../../results/err_est_outputs_070521_EALSTM_fold"+str(k)+".feather")
+	# ea_df = pd.read_feather("../../results/err_est_outputs_070221_EALSTM_fold"+str(k)+".feather")
 
 	ea_df.drop(ea_df[ea_df['Date'] < gb_date_df['Date'].min()].index,axis=0,inplace=True)
 	pdb.set_trace()
@@ -82,5 +83,5 @@ for i,site_id in enumerate(site_ids):
 	per_site_df = per_site_df.append(site_df)
 
 per_site_df.reset_index(inplace=True)
-per_site_df.to_csv("../../results/err_per_site_070521.csv")
-per_site_df.to_feather("../../results/err_per_site_070521.feather")
+per_site_df.to_csv("../../results/err_per_site_070621.csv")
+per_site_df.to_feather("../../results/err_per_site_070621.feather")
