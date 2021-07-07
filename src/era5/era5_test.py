3a6bef64e0679777044f83b1f428e990815c13ce
willa099@umn.edu <willa099@umn.edu>
2021-07-07 16:22:48 -0500

    move to remote

--- src/era5/era5_test.py
+++ src/era5/era5_test.py
@@ -3,15 +3,21 @@ import numpy as np
 import pandas as pd
 import pygrib
 import pdb
+import math
 
-site_ids = ['nhdhr_']
-
+site_ids = ['nhdhr_143249470']
 metadata = pd.read_csv('../../metadata/lake_metadata.csv')
 metadata.set_index('site_id',inplace=True)
 pdb.set_trace()
+factor = 2 
 for site_id in site_ids:
 
-
+    lat = metadata.loc[site_id]['lake_lat_deg']
+    lon = metadata.loc[site_id]['lake_lon_deg']
+    lat_upper = math.ceil(lat * factor) / factor
+    lat_lower = math.floor(lat * factor) / factor
+    lon_upper = math.ceil(lon * factor) / factor
+    lon_lower = math.floor(lon * factor) / factor
     c = cdsapi.Client()
 
     fn = 'download.grib'
@@ -28,13 +34,14 @@ for site_id in site_ids:
             'day': '01',
             'time': '00:00',
             'area': [
-                45, -93.21, 44.99,
-                -93.22,
+                lat_upper, lon_upper, lat_lower,
+                lon_lower
             ],
         },
         fn)
 
     gr = pygrib.open(fn)
     for g in gr:
+        pdb.set_trace()
         print(g)
         print(g.values)
