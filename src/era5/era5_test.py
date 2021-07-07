9dff290de4abf44bec8400fefa8bdd64678fb5b4
willa099@umn.edu <willa099@umn.edu>
2021-07-07 15:49:57 -0500

    move to remote

---
+++ src/era5/era5_test.py
@@ -0,0 +1,29 @@
+import cdsapi
+import numpy as np
+import pandas as pd
+
+
+
+c = cdsapi.Client()
+
+gr = c.retrieve(
+    'reanalysis-era5-land',
+    {
+        'format': 'grib',
+        'variable': [
+            'lake_mix_layer_temperature', 'skin_temperature',
+        ],
+        'year': '2020',
+        'month': '07',
+        'day': '01',
+        'time': '00:00',
+        'area': [
+            45, -93.21, 44.99,
+            -93.22,
+        ],
+    },
+    'download.grib')
+
+for g in gr:
+    print(g)
+    print(g.values)
