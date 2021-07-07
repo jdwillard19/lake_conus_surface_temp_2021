146010fbc76ad273f67861898af5e89d37ed2da4
willa099@umn.edu <willa099@umn.edu>
2021-07-07 16:17:25 -0500

    move to remote

--- src/era5/era5_test.py
+++ src/era5/era5_test.py
@@ -11,28 +11,29 @@ pdb.set_trace()
 for site_id in site_ids:
 
 
-c = cdsapi.Client()
+    c = cdsapi.Client()
 
-fn = 'download.grib'
-c.retrieve(
-    'reanalysis-era5-land',
-    {
-        'format': 'grib',
-        'variable': [
-            'lake_mix_layer_temperature', 'skin_temperature',
-        ],
-        'year': '2020',
-        'month': '07',
-        'day': '01',
-        'time': '00:00',
-        'area': [
-            45, -93.21, 44.99,
-            -93.22,
-        ],
-    },
-    fn)
+    fn = 'download.grib'
 
-gr = pygrib.open(fn)
-for g in gr:
-    print(g)
-    print(g.values)
+    c.retrieve(
+        'reanalysis-era5-land',
+        {
+            'format': 'grib',
+            'variable': [
+                'lake_mix_layer_temperature', 'skin_temperature',
+            ],
+            'year': '2020',
+            'month': '07',
+            'day': '01',
+            'time': '00:00',
+            'area': [
+                45, -93.21, 44.99,
+                -93.22,
+            ],
+        },
+        fn)
+
+    gr = pygrib.open(fn)
+    for g in gr:
+        print(g)
+        print(g.values)
