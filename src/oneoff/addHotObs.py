1f2ca5172f4ea683f2ca0cba7eadc87343cb36c0
willa099@umn.edu <willa099@umn.edu>
2021-07-29 19:40:40 -0500

    addHot debug

--- src/oneoff/addHotObs.py
+++ src/oneoff/addHotObs.py
@@ -6,4 +6,6 @@ k_arr = np.arange(5)+1
 
 for k in k_arr:
 	data = np.load("../evaluate/ealstm_trn_data_072621_5fold_k"+str(int(k))+".npy")
-	pdb.set_trace()
+	augment = np.repeat(data[ind1,:,:],10,axis=0)
+	data = np.concatenate((data,augment), axis=0)
+	np.save("../evaluate/ealstm_trn_data_072621_hotaug_k"+str(int(k))+".npy",data)
