9c049b5b6b9a63dc832fd639417dfdd278bcef85
willa099@umn.edu <willa099@umn.edu>
2021-08-01 16:22:26 -0700

    debug

--- src/hpc/create_oversamp_jobs.py
+++ src/hpc/create_oversamp_jobs.py
@@ -27,16 +27,16 @@ for name in folds_arr:
     print(name)
     l = name
 
-    header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=EALSTM_%s_oversamp.out\n#SBATCH --error=EALSTM_%s_oversamp.err\n#SBATCH --gres=gpu:v100:1\n#SBATCH -p v100"%(l,l)
+    header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=EALSTM_%s_oversamp1.out\n#SBATCH --error=EALSTM_%s_oversamp1.err\n#SBATCH --gres=gpu:v100:1\n#SBATCH -p v100"%(l,l)
     script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
-    script2 = "python EALSTM_err_est_oversamp.py %s"%(l)
+    script2 = "python EALSTM_err_est_oversamp1.py %s"%(l)
     # script3 = "python singleModel_customSparse.py %s"%(l)
     all= "\n".join([header,script,script2])
-    sbatch = "\n".join(["sbatch job_%s_foldEAoversamp.sh"%(l),sbatch])
-    with open('../../hpc/job_{}_foldEAoversamp.sh'.format(l), 'w') as output:
+    sbatch = "\n".join(["sbatch job_%s_foldEAoversamp1.sh"%(l),sbatch])
+    with open('../../hpc/job_{}_foldEAoversamp1.sh'.format(l), 'w') as output:
         output.write(all)
 
-compile_job_path= '../../hpc/sbatch_script_err_est_ea_oversamp.sh'
+compile_job_path= '../../hpc/sbatch_script_err_est_ea_oversamp1.sh'
 with open(compile_job_path, 'w') as output2:
     output2.write(sbatch)
 print(compile_job_path)
