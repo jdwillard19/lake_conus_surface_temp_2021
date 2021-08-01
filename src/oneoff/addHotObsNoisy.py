1f9a6b8b8f0fd7ad2351f61e77c3148422a7ca5b
willa099@umn.edu <willa099@umn.edu>
2021-08-01 16:16:59 -0700

    debug

--- src/oneoff/addHotObsNoisy.py
+++ src/oneoff/addHotObsNoisy.py
@@ -6,11 +6,14 @@ k_arr = np.arange(5)+1
 
 for k in k_arr:
 	data = np.load("../evaluate/ealstm_trn_data_072621_5fold_k"+str(int(k))+".npy")
-	ind1 = np.where(data[:,:,-1]>33)[0]
-	augment = data[ind1,:,:]
+	ind1 = np.where(data[:,:,-1]>32)[0]
+	# augment = data[ind1,:,:]
+	augment = np.repeat(data[ind1,:,:],10,axis=0)
 
-	pdb.set_trace()
-	#remove nonhot obs from augset
-	# data[ind1,:,:-1] = x + (0.1**0.5)*torch.randn(5, 10, 20)	
+
+	#remove non-hot obs in augment
+	ind3 = np.where(augment[:,:,-1] < 32)
+	augment[ind3] = np.nan
+
 	data = np.concatenate((data,augment), axis=0)
-	np.save("../evaluate/ealstm_trn_data_072621_hotaug_k"+str(int(k))+".npy",data)
+	np.save("../evaluate/ealstm_trn_data_oversamp1_k"+str(int(k))+".npy",data)
