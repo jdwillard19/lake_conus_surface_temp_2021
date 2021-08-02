cc9df30654d45b95b1ee69a9d3ba4ecb939d1288
willa099@umn.edu <willa099@umn.edu>
2021-08-01 17:19:51 -0700

    oversamp3

---
+++ src/oneoff/addData_oversamp3.py
@@ -0,0 +1,22 @@
+import pdb
+import numpy as np
+# from imblearn.over_sampling import SMOTE 
+
+k_arr = np.arange(5)+1
+
+for k in k_arr:
+	data = np.load("../evaluate/ealstm_trn_data_072621_5fold_k"+str(int(k))+".npy")
+	ind1 = np.where(data[:,:,-1]>33)[0]
+	# augment = data[ind1,:,:]
+	augment = np.repeat(data[ind1,:,:],20,axis=0)
+
+
+	#remove non-hot obs in augment
+	ind3 = np.where(augment[:,:,-1] < 33)
+	augment[ind3] = np.nan
+
+	augment[:,:,:-1] = augment[:,:,:-1] + (.1**.5)*np.random.randn(augment.shape[0],augment.shape[1],augment.shape[2]-1)
+	augment[:,:,-1] = augment[:,:,-1] + np.random.randn(augment.shape[0],augment.shape[1])
+
+	data = np.concatenate((data,augment), axis=0)
+	np.save("../evaluate/ealstm_trn_data_oversamp3_k"+str(int(k))+".npy",data)
