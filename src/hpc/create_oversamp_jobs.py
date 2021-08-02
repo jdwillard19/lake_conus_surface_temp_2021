c69d5c409be327f5102aee427c90849b86760eb8
willa099@umn.edu <willa099@umn.edu>
2021-08-02 11:13:44 -0700

    recreate jobs

--- src/hpc/create_oversamp_jobs.py
+++ src/hpc/create_oversamp_jobs.py
@@ -42,32 +42,33 @@ folds_arr = np.arange(5)+1
 # print(compile_job_path)
 
 
-# for name in folds_arr:
-#     ct += 1
-#     #for each unique lake
-#     print(name)
-#     l = name
+for name in folds_arr:
+    ct += 1
+    #for each unique lake
+    print(name)
+    l = name
 
-#     header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n\
-#                               #SBATCH --mem=20g\n\
-#                               #SBATCH --mail-type=ALL\n\
-#                               #SBATCH --mail-user=willa099@umn.edu\n\
-#                               #SBATCH --output=EALSTM_%s_oversamp2.out\n\
-#                               #SBATCH --error=EALSTM_%s_oversamp2.err\n\
-#                               #SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
-#     script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
-#     script2 = "python EALSTM_err_est_oversamp2.py %s"%(l)
-#     all= "\n".join([header,script,script2])
-#     sbatch = "\n".join(["sbatch job_%s_foldEAoversamp2.sh"%(l),sbatch])
-#     with open('../../hpc/job_{}_foldEAoversamp2.sh'.format(l), 'w') as output:
-#         output.write(all)
+    header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n\
+                              #SBATCH --mem=20g\n\
+                              #SBATCH --mail-type=ALL\n\
+                              #SBATCH --mail-user=willa099@umn.edu\n\
+                              #SBATCH --output=EALSTM_%s_oversamp2.out\n\
+                              #SBATCH --error=EALSTM_%s_oversamp2.err\n\
+                              #SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
+    script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
+    script2 = "python EALSTM_err_est_oversamp2.py %s"%(l)
+    all= "\n".join([header,script,script2])
+    sbatch = "\n".join(["sbatch job_%s_foldEAoversamp2.sh"%(l),sbatch])
+    with open('../../hpc/job_{}_foldEAoversamp2.sh'.format(l), 'w') as output:
+        output.write(all)
 
-# compile_job_path= '../../hpc/sbatch_script_err_est_ea_oversamp2.sh'
-# with open(compile_job_path, 'w') as output2:
-#     output2.write(sbatch)
-# print(compile_job_path)
+compile_job_path= '../../hpc/sbatch_script_err_est_ea_oversamp2.sh'
+with open(compile_job_path, 'w') as output2:
+    output2.write(sbatch)
+print(compile_job_path)
 
-
+sbatch = ""
+ct = 0
 
 for name in folds_arr:
     ct += 1
@@ -88,6 +89,9 @@ for name in folds_arr:
     sbatch = "\n".join(["sbatch job_%s_foldEAoversamp3.sh"%(l),sbatch])
     with open('../../hpc/job_{}_foldEAoversamp3.sh'.format(l), 'w') as output:
         output.write(all)
+
+sbatch = ""
+ct = 0
 
 compile_job_path= '../../hpc/sbatch_script_err_est_ea_oversamp3.sh'
 with open(compile_job_path, 'w') as output2:
