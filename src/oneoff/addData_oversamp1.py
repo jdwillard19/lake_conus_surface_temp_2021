1fc80778acf5eeb31d23f3529decc94ffa648d34
willa099@umn.edu <willa099@umn.edu>
2021-08-01 18:10:06 -0700

    ugh

--- src/oneoff/addData_oversamp1.py
+++ src/oneoff/addData_oversamp1.py
@@ -14,8 +14,9 @@ for k in k_arr:
 	#remove non-hot obs in augment
 	ind3 = np.where(augment[:,:,-1] < 32)
 	# augment[ind3] = np.nan
-	pdb.set_trace()
-	augment[ind3,-1] = np.nan
+	# pdb.set_trace()
+	augment[ind3[0],ind3[1],-1]
+	# augment[ind3,-1] = np.nan
 	data = np.concatenate((data,augment), axis=0)
 	pdb.set_trace()
 	np.save("../evaluate/ealstm_trn_data_oversamp1_k"+str(int(k))+".npy",data)
