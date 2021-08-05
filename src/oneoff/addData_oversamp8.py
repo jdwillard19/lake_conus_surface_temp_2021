2c497678efb7f2bc22845f532ad3ccba7843f752
willa099@umn.edu <willa099@umn.edu>
2021-08-05 08:51:56 -0700

    os78

--- src/oneoff/addData_oversamp8.py
+++ src/oneoff/addData_oversamp8.py
@@ -8,15 +8,15 @@ for k in k_arr:
 	data = np.load("../evaluate/ealstm_trn_data_072621_5fold_k"+str(int(k))+".npy")
 	ind1 = np.where(data[:,:,-1]>32)[0]
 	# augment = data[ind1,:,:]
-	augment = np.repeat(data[ind1,:,:],15,axis=0)
+	augment = np.repeat(data[ind1,:,:],10,axis=0)
 
 
 	#remove non-hot obs in augment
 	ind3 = np.where(augment[:,:,-1] < 32)
 	augment[ind3[0],ind3[1],-1] = np.nan
 
-	augment[:,:,:-1] = augment[:,:,:-1] + (.05**.5)*np.random.randn(augment.shape[0],augment.shape[1],augment.shape[2]-1)
-	augment[:,:,-1] = augment[:,:,-1] + (.5**.5)*np.random.randn(augment.shape[0],augment.shape[1])
+	augment[:,:,:-1] = augment[:,:,:-1] + (.025**.5)*np.random.randn(augment.shape[0],augment.shape[1],augment.shape[2]-1)
+	augment[:,:,-1] = augment[:,:,-1] + (.25**.5)*np.random.randn(augment.shape[0],augment.shape[1])
 
 	data = np.concatenate((data,augment), axis=0)
-	np.save("../evaluate/ealstm_trn_data_oversamp5_k"+str(int(k))+".npy",data)
+	np.save("../evaluate/ealstm_trn_data_oversamp8_k"+str(int(k))+".npy",data)
