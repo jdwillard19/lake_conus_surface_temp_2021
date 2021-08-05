b66dfef3af0cd6fc3ea093cc6368c671ea3677e3
willa099@umn.edu <willa099@umn.edu>
2021-08-05 07:55:26 -0700

    compile os

--- src/oneoff/compileErrEstResults_oversamp.py
+++ src/oneoff/compileErrEstResults_oversamp.py
@@ -54,6 +54,10 @@ for k in folds_arr: #CHANGE DIS----------------
 
 	#oversamp5
 	ea_df = pd.read_feather("../../results/err_est_outputs_072621_EALSTM_fold"+str(k)+"_oversamp5.feather")
+
+	#oversamp6
+	ea_df = pd.read_feather("../../results/err_est_outputs_072621_EALSTM_fold"+str(k)+"_oversamp6.feather")
+	
 	# pdb.set_trace()
 	# ea_df.drop(ea_df[ea_df['Date'] < gb_date_df['Date'].min()].index,axis=0,inplace=True)
 	# assert ea_df.shape[0] == lm_df.shape[0]
