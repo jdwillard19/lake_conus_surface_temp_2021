9f963cd46aa624dd9b839477ae8b16396ffdd8b6
willa099@umn.edu <willa099@umn.edu>
2021-08-25 19:42:31 -0700

    vis high temps

--- src/oneoff/visHighTemp.py
+++ src/oneoff/visHighTemp.py
@@ -12,10 +12,19 @@ site_ids = ['nhdhr_112699096']
 dates = np.load("../../data/processed/"+site_ids[0]+"/dates.npy",allow_pickle=True)
 
 for site_id in site_ids:
-	obs = np.load("../../data/processed/"+site_id+"/obs.npy")
 	feats = np.load("../../data/processed/"+site_id+"/features.npy")
-	air_temp = feats[:,6]
+	res = pd.read_feather("../../results/SWT_results/outputs_"+site_id+'.feather')
+	pred = res['temp_pred'].values
+	obs = res['temp_actual'].values
+	air_temp = feats[:,6] - 273.15
+	print("max air temp: ",air_temp.max())
 	for y in range(1980,2021):
-		pdb.set_trace()
+		start_date = pd.Timestamp(str(y)+'-04-01').to_datetime64()
+		end_date = pd.Timestamp(str(y)+'-09-01').to_datetime64()
+		start_ind = np.where(dates==start_date)[0][0]
+		end_ind = np.where(dates==end_date)[0][0]
+
+		p_obs = obs[start_ind:end_ind]
+		p_at = air_temp[start_ind:end_ind]
 		start_ind = 0
 
