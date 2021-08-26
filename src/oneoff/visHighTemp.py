f3074aba96efb039aa35047a83a13de426defddc
willa099@umn.edu <willa099@umn.edu>
2021-08-25 19:21:06 -0700

    vis high temps

---
+++ src/oneoff/visHighTemp.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import pdb
+
+
+#
+site_ids = ['nhdhr_112699096']
+
+
+
+#load data
+dates = "../../data/processed/"+site_id+"/dates"
+
+for site_id in site_ids:
+	obs = np.load("../../data/processed/"+site_id+"/obs.npy")
+	feats = np.load("../../data/processed/"+site_id+"/features.npy")
+	air_temp = feats[:,6]
+	for y in range(1980,2021):
+		pdb.set_trace()
+		start_ind = 0
+
