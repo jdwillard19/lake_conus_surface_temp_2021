de1aa64d461791ea515bc354dbf398fe4a14c8ac
willa099@umn.edu <willa099@umn.edu>
2021-08-25 20:58:50 -0700

    vis high temps

--- src/oneoff/visHighTemp.py
+++ src/oneoff/visHighTemp.py
@@ -19,31 +19,41 @@ for site_id in site_ids:
 	# res = pd.read_feather("../../results/SWT_results/outputs_"+site_id+'.feather')
 
 	#get fold
-	fold = meta[meta['site_id']==site_id].group_id.values[0]
+	fold = meta[meta['site_id']==site_id].group_id.values[0]-1
 	res = pd.read_feather("../../results/err_est_outputs_072621_EALSTM_fold"+str(fold)+"_oversamp_norm2.feather")
 	# pred = res['temp_pred'].values
 	# obs = res['temp_actual'].values
 	air_temp = feats[:,6] - 273.15
 	print("max air temp: ",air_temp.max())
 	for y in range(1980,2021):
+		print("year: ",y)
 		start_date = pd.Timestamp(str(y)+'-04-01').to_datetime64()
 		end_date = pd.Timestamp(str(y)+'-09-01').to_datetime64()
 		start_ind = np.where(dates==start_date)[0][0]
 		end_ind = np.where(dates==end_date)[0][0]
-		p_dates = [str(d)[:10] for d in dates[start_ind:end_ind]]
-		pdb.set_trace()
-		p_obs = obs[start_ind:end_ind]
+		p_dates = np.array([str(d)[:10] for d in dates[start_ind:end_ind]])
+		p_pred = res[(res['site_id'] == site_id)&(res['Date'] < end_date)&(res['Date'] > start_date)]['wtemp_predicted'].values
+		p_obs = res[(res['site_id'] == site_id)&(res['Date'] < end_date)&(res['Date'] > start_date)]['wtemp_actual'].values
+		obs_dates = res[(res['site_id'] == site_id)&(res['Date'] < end_date)&(res['Date'] > start_date)]['Date'].values
+		n_obs = obs.shape[0]
+		if n_obs == 0:
+			continue
+		
+		p_x = []
+		for i in range(n_obs):
+			p_x.append(np.where(p_dates==obs_dates[i])[0][0])
+
 		if np.isnan(p_obs).all():
 			continue
 		x = np.arange(end_ind-start_ind)
 		p_at = air_temp[start_ind:end_ind]
-		p_pred = pred[start_ind:end_ind]
+		# p_pred = pred[start_ind:end_ind]
 
 		x_p_obs = x[np.isfinite(p_obs)]
 		y_p_obs = p_obs[np.isfinite(p_obs)]
-		plt.plot(p_pred,color='green',label='EALSTM prediction')
+		plt.scatter(p_x,p_pred,color='green',label='EALSTM prediction')
 		plt.plot(p_at,color='blue',label='Air Temperature')
-		plt.scatter(x_p_obs,y_p_obs,c='red',marker='+',s=15,label='Observation')
+		plt.scatter(p_x,p_obs,c='red',marker='+',s=15,label='Observation')
 		plt.xticks(ticks=x, labels=p_dates)
 		plt.locator_params(axis='x', nbins=4)
 		plt.xlabel("Day of Summer")
