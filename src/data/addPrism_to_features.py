2434ea93b713f1f313e09882b2fc1dbc34283288
willa099@umn.edu <willa099@umn.edu>
2022-01-04 16:00:35 -0600

    debug prism data process

--- src/data/addPrism_to_features.py
+++ src/data/addPrism_to_features.py
@@ -1,3 +1,50 @@
+# import pandas as pd
+# import sys
+# import numpy as np
+# import pdb
+# import rasterio
+# import os
+
+# #get prism data to align with dates
+# metadata = pd.read_csv("../../metadata/lake_metadata.csv")
+# start = int(sys.argv[1])
+# end = int(sys.argv[2])
+# site_ids = metadata['site_id'].values[start:end]
+# no_ct = 0
+# yes_ct = 0
+# for site_ct, site_id in enumerate(site_ids):
+#     print(site_ct,"/",len(site_ids)," starting ", site_id)
+#     feat_path = "../../data/processed/"+site_id+"/features.npy"
+#     feat_old = np.load(feat_path, allow_pickle=True)
+
+#     dates_path = "../../data/processed/"+site_id+"/dates.npy"
+#     dates = np.load(dates_path, allow_pickle=True)
+
+#     new_temps = np.empty(feat_old.shape[0])
+#     lat = feat_old[0,1]
+#     lon = feat_old[0,2]
+#     print("yes ct: ", yes_ct)
+#     print("no ct: ", no_ct)
+#     for date_ct, date in enumerate(dates):
+#         print("date ", date)
+#         date_str = str(date)[:4]+str(date)[5:7]+str(date)[8:10]
+#         file_path = '../../data/raw/prism/PRISM_tmean_stable_4kmD2_'+date_str+'_bil.bil'
+
+#         # if date_ct > 500:
+#         #     pdb.set_trace()
+#         if os.path.exists(file_path):
+#             dataset = rasterio.open(file_path)
+#             band1 = dataset.read(1)
+#             band1[band1==-9999] = np.nan #to avoid errors
+
+#             py, px = dataset.index(lon, lat)
+#             at = band1[py,px]
+#             if np.isnan(at):
+#                 no_ct += 1
+#             else:
+#                 yes_ct += 1
+#         else:
+#             print("date not in PRISM")
 import pandas as pd
 import sys
 import numpy as np
@@ -12,36 +59,43 @@ end = int(sys.argv[2])
 site_ids = metadata['site_id'].values[start:end]
 no_ct = 0
 yes_ct = 0
-for site_ct, site_id in enumerate(site_ids):
-    print(site_ct,"/",len(site_ids)," starting ", site_id)
-    feat_path = "../../data/processed/"+site_id+"/features.npy"
-    feat_old = np.load(feat_path, allow_pickle=True)
+site_id0 = site_ids[0]
 
-    dates_path = "../../data/processed/"+site_id+"/dates.npy"
-    dates = np.load(dates_path, allow_pickle=True)
+feat_path = "../../data/processed/"+site_id0+"/features.npy"
+feat_old = np.load(feat_path, allow_pickle=True)
 
-    new_temps = np.empty(feat_old.shape[0])
-    lat = feat_old[0,1]
-    lon = feat_old[0,2]
-    print("yes ct: ", yes_ct)
-    print("no ct: ", no_ct)
-    for date_ct, date in enumerate(dates):
-        print("date ", date)
-        date_str = str(date)[:4]+str(date)[5:7]+str(date)[8:10]
-        file_path = '../../data/raw/prism/PRISM_tmean_stable_4kmD2_'+date_str+'_bil.bil'
+dates_path = "../../data/processed/"+site_id0+"/dates.npy"
+dates = np.load(dates_path, allow_pickle=True)
 
-        # if date_ct > 500:
-        #     pdb.set_trace()
-        if os.path.exists(file_path):
-            dataset = rasterio.open(file_path)
-            band1 = dataset.read(1)
-            band1[band1==-9999] = np.nan #to avoid errors
+new_temps = np.empty(feat_old.shape[0])
 
-            py, px = dataset.index(lon, lat)
-            at = band1[py,px]
-            if np.isnan(at):
-                no_ct += 1
-            else:
-                yes_ct += 1
+print("yes ct: ", yes_ct)
+print("no ct: ", no_ct)
+for date_ct, date in enumerate(dates):
+    print("date ", date)
+    date_str = str(date)[:4]+str(date)[5:7]+str(date)[8:10]
+    file_path = '../../data/raw/prism/PRISM_tmean_stable_4kmD2_'+date_str+'_bil.bil'
+
+    # if date_ct > 500:
+    #     pdb.set_trace()
+    if os.path.exists(file_path):
+        dataset = rasterio.open(file_path)
+        band1 = dataset.read(1)
+        band1[band1==-9999] = np.nan #to avoid errors
+
+    for site_ct, site_id in enumerate(site_ids):
+        print(site_ct,"/",len(site_ids)," starting ", site_id)
+
+        pdb.set_trace()
+
+        lon = 0
+        lat = 0
+        py, px = dataset.index(lon, lat)
+        at = band1[py,px]
+        if np.isnan(at):
+            no_ct += 1
+        else:
+            pdb.set_trace()
+            yes_ct += 1
         else:
             print("date not in PRISM")
