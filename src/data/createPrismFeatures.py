8c49b7d640087ca21c05be98027f0f085a432eda
willa099@umn.edu <willa099@umn.edu>
2022-01-06 10:19:34 -0600

    add prism to features debug

--- src/data/createPrismFeatures.py
+++ src/data/createPrismFeatures.py
@@ -5,6 +5,7 @@ import pdb
 import rasterio
 import os
 
+TO_KELVIN = 273.15
 #load metadata
 metadata = pd.read_csv("../../metadata/lake_metadata.csv")
 metadata = metadata[metadata['num_obs'] > 0]
@@ -29,11 +30,32 @@ for i in range(300):
     load_at = np.load("../../data/raw/prism/ats_split"+str(i)+".npy",allow_pickle=True)
     at_arr[:,inds_arr[i]] = load_at
 
+#repreprocess per site
 for site_ct, site_id in enumerate(site_ids):
     print(site_ct,"/",len(site_ids)," starting ", site_id)
     feat_path = "../../data/processed/"+site_id+"/features.npy"
-    feat_old = np.load(feat_path, allow_pickle=True)
+    obs_path = "../../data/processed/"+site_id+"/obs.npy"
+    dates_path = "../../data/processed/"+site_id+"/dates.npy"
 
-    new_feat_path = "../../data/processed/"+site_id+"/features_wPrism.npy"
-    pdb.set_trace()
+    feat = np.load(feat_path, allow_pickle=True)
+    obs = np.load(obs_path, allow_pickle=True)
+    dates = np.load(dates_path, allow_pickle=True)
+
+    new_feat_path = "../../data/processed/"+site_id+"/features_wPrism"
+    new_obs_path = "../../data/processed/"+site_id+"/obs_wPrism"
+    new_dates_path = "../../data/processed/"+site_id+"/dates_wPrism"
+
+    feat[:,6] = at_arr[site_ct,:]+TO_KELVIN
+
+    #clip to prism data range (exclude 1980)
+    feat = feat[366:]
+    obs = obs[366:]
+    dates = dates[366:]
+
+    assert np.isfinite(feat).all()
+
+    #save
+    np.save(new_feat_path,feat)
+    np.save(new_obs_path,obs)
+    np.save(new_dates_path,dates)
 
